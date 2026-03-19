b89c64d7873b3b1463c4398fbe0c73aa6c62027d
Peter <peter@ezgrid.cn>
2026-03-19 17:16:09 +0800

    重构代码，创建 Lambda 函数包装器

---
+++ packages/core/src/util/index.ts
@@ -0,0 +1,35 @@
+import { Context, APIGatewayProxyEvent } from "aws-lambda";
+
+//
+
+export module Util {
+  /* handler 是 Lambda 函数的包装器
+   *
+   * 我们希望集中处理所有 Lambda 函数中的错误。
+   * 最后，由于我们所有的 Lambda 函数都将处理 API 端点，我们希望在一个地方统一处理 HTTP 响应。
+   */
+  export function handler(
+    lambda: (evt: APIGatewayProxyEvent, context: Context) => Promise<string>
+  ) {
+    return async function (event: APIGatewayProxyEvent, context: Context) {
+      let body: string, statusCode: number;
+
+      try {
+        // Run the Lambda
+        body = await lambda(event, context);
+        statusCode = 200;
+      } catch (error) {
+        statusCode = 500;
+        body = JSON.stringify({
+          error: error instanceof Error ? error.message : String(error),
+        });
+      }
+
+      // Return HTTP response
+      return {
+        body,
+        statusCode,
+      };
+    };
+  }
+}
--- packages/functions/src/create.ts
+++ packages/functions/src/create.ts
@@ -1,19 +1,25 @@
 import * as uuid from "uuid";
 import { Resource } from "sst";
-import { APIGatewayProxyEvent } from "aws-lambda";
+import { Util } from "@sst-notes/core/util";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { PutCommand, DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 
 const dynamoDb = DynamoDBDocumentClient.from(new DynamoDBClient({}));
 
-export async function main(event: APIGatewayProxyEvent) {
-  let data, params;
+export const main = Util.handler(
+  // Lambda 函数 async，并简单地返回结果
+  async (event) => {
+    let data = {
+      content: "",
+      attachment: "",
+    };
 
-  // Request body is passed in as a JSON encoded string in 'event.body'
-  // 解析来自 event.body 的输入。这代表 HTTP request body。
-  if (event.body) {
-    data = JSON.parse(event.body);
-    params = {
+    // 解析来自 event.body 的输入。这代表 HTTP request body。
+    if (event.body != null) {
+      data = JSON.parse(event.body);
+    }
+
+    const params = {
       TableName: Resource.Notes.name, // 通过 Resource.Notes.name 使用 SST SDK 访问我们关联的 DynamoDB 表
       Item: {
         // The attributes of the item to be created
@@ -24,31 +30,9 @@ export async function main(event: APIGatewayProxyEvent) {
         createdAt: Date.now(), // Current Unix timestamp
       },
     };
-  } else {
-    return {
-      statusCode: 404,
-      body: JSON.stringify({ error: true }),
-    };
-  }
 
-  try {
-    // 调用 DynamoDB，插入一个新对象
     await dynamoDb.send(new PutCommand(params));
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify(params.Item),
-    };
-  } catch (error) {
-    let message;
-    if (error instanceof Error) {
-      message = error.message;
-    } else {
-      message = String(error);
-    }
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: message }),
-    };
+    return JSON.stringify(params.Item);
   }
-}
+);
