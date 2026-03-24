3ac87360d2bcee71182ef2506c12d8a345b02075
Peter <peter@ezgrid.cn>
2026-03-24 14:38:08 +0800

    8.2 Handle CORS in S3 for File Uploads

--- infra/storage.ts
+++ infra/storage.ts
@@ -1,5 +1,7 @@
 // Create an S3 bucket
-export const bucket = new sst.aws.Bucket("Uploads");
+export const bucket = new sst.aws.Bucket("Uploads", {
+  // CORS Enabled by default
+});
 
 // Create the DynamoDB table
 export const table = new sst.aws.Dynamo("Notes", {
