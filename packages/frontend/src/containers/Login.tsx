c941864e741b12e4dd1b5c51a9dabc79a964c8c8
Peter <peter@ezgrid.cn>
2026-03-25 11:08:29 +0800

    11.2.2 Add the session to the state

--- packages/frontend/src/containers/Login.tsx
+++ packages/frontend/src/containers/Login.tsx
@@ -4,8 +4,11 @@ import Stack from "react-bootstrap/Stack";
 import Button from "react-bootstrap/Button";
 import { Auth } from "aws-amplify";
 import "./Login.css";
+import { useAppContext } from "../lib/contextLib";
 
 export default function Login() {
+  const { userHasAuthenticated } = useAppContext();
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
@@ -18,7 +21,7 @@ export default function Login() {
 
     try {
       await Auth.signIn(email, password);
-      alert("Logged in");
+      userHasAuthenticated(true);
     } catch (error) {
       // Prints the full error
       console.error(error);
